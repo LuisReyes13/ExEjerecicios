0ba5a35bd6ea33e7e240434bf6d81db5be69d18b
luis_ <luis_@LENOVO>
2023-03-10 20:09:27 -0600

    Menu p1

--- ExEjerecicios/Program.cs
+++ ExEjerecicios/Program.cs
@@ -2,25 +2,4 @@
 using System;
 
 Console.ForegroundColor = ConsoleColor.Green;
-
-//SumNum3 ej7 = new SumNum3();
-//ej7.verSuma();
-
-//NumMayor3 ej6 = new NumMayor3();
-//ej6.Mayor();
-
-//Ordenarcn ej5 = new Ordenarcn();
-//ej5.ordenarnumlinq();
-
-//Impnimpar ej4 = new Impnimpar();
-//ej4.nImpar();
-
-//Sumatoria_M5 ej3 = new Sumatoria_M5();
-//ej3.sumatoria();
-
-Division ej2 = new Division(5,3);
-//ej2.division();
-Console.WriteLine(ej2._cociente);
-
-//Par_impar Ej1 = new Par_impar();
-//Ej1.numero();
+Utilerias.menu();
--- ExEjerecicios/Utilerias.cs
+++ ExEjerecicios/Utilerias.cs
@@ -42,11 +42,68 @@ namespace ExEjerecicios
             } while (!esNumero);
             return n;
         }
-        public static int pedirnum()
+        public static void menu()
         {
-            Console.Write("Ingresar un numero: ");
-            int n = int.Parse(Console.ReadLine());
-            return n;
+            Par_impar ej1 = new Par_impar();
+            Division ej2 = new Division();
+            Sumatoria_M5 ej3 = new Sumatoria_M5();
+            Impnimpar ej4 = new Impnimpar();
+            Ordenarcn ej5 = new Ordenarcn();
+            NumMayor3 ej6 = new NumMayor3();
+            SumNum3 ej7 = new SumNum3();
+
+            bool salir = false;
+
+            while (!salir)
+            {
+                opcionesmenu();
+                int opcion = validarint("Elige una de las opciones: ");
+
+                switch (opcion)
+                {
+                    case 1:
+                        ej1.numero();
+                        break;
+                    case 2:
+                        ej2.division();
+                        break;
+                    case 3:
+                        ej3.sumatoria();
+                        break;
+                    case 4:
+                        ej4.nImpar();
+                        break;
+                    case 5:
+                        ej5.ordenarnumlinq();
+                        break;
+                    case 6:
+                        ej6.Mayor();
+                        break;
+                    case 7:
+                        ej7.verSuma();
+                        break;
+                    case 8:
+                        Console.WriteLine("Has elegido salir de la aplicación, presiona ENTER");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 8");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static void opcionesmenu()
+        {
+            Console.WriteLine("1. Lee un numero y dice si es par o impar");
+            Console.WriteLine("2. Lee 2 y divide el mayor entre el menor");
+            Console.WriteLine("3. Suma numeros enteros multiplos de 5 entre 1 y 100");
+            Console.WriteLine("4. Imprime numeros impar en orden descendente entre 1 y 100");
+            Console.WriteLine("5. Ordena numeros establecidos");
+            Console.WriteLine("6. Lee 3 numeros y determina cual es mayor");
+            Console.WriteLine("7. Lee 3 numeros y determina si uno es la suma de los otros dos");
+            Console.WriteLine("8. Salir");
         }
     }
 }
